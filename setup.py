73fd141deeb7b4248b5932688f85bec29f078f9c
Jonathan Emord <jonathan.emord@gmail.com>
2017-12-17 19:09:17 -0500

    Cythonize packages.

--- setup.py
+++ setup.py
@@ -1,6 +1,31 @@
 from setuptools import setup
 import io
 
+from setuptools.extension import Extension
+
+try:
+    import Cython
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
+
+ext = '.pyx' if USE_CYTHON else '.c'
+extensions = [
+    Extension('jsonobject', ["jsonobject/*.pyx"],),
+    Extension('jsonobject.api', ["jsonobject/api.pyx"],),
+    Extension('jsonobject.base', ["jsonobject/base.pyx"],),
+    Extension('jsonobject.base_properties', ["jsonobject/base_properties.pyx"],),
+    Extension('jsonobject.containers', ["jsonobject/containers.pyx"],),
+    Extension('jsonobject.exceptions', ["jsonobject/exceptions.pyx"],),
+    Extension('jsonobject.properties', ["jsonobject/properties.pyx"],),
+    Extension('jsonobject.utils', ["jsonobject/utils.pyx"],),
+]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+
 with io.open('README.md', 'rt', encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
@@ -13,7 +38,11 @@ setup(
     long_description=long_description,
     url='https://github.com/dannyroberts/jsonobject',
     packages=['jsonobject'],
-    install_requires=['six'],
+    install_requires=[
+        'six',
+        'cython'
+    ],
     tests_require=['unittest2'],
+    ext_modules=extensions,
     test_suite='test',
 )
